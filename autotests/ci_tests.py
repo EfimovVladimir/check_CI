b7722d3ba92cfa86793fbe6f48f46f371c6c9672
Vladimir <mailevv@gmail.com>
2020-08-14 12:09:47 +0300

    Update ci_tests.py

--- autotests/ci_tests.py
+++ autotests/ci_tests.py
@@ -52,18 +52,6 @@ while 1:
 tst_command = 'gradle --no-daemon cleanTest test -Dcur_app=%(app)s --tests "com.twosigma.beakerx.autotests.%(tst)s" --info' % { "app" : cur_app, "tst" : tst_templ }
 print(tst_command)
 result = subprocess.call(tst_command, shell=True)
-
-# kill unused processes
-if platform.system() == 'Windows':
-    for proc in psutil.process_iter():
-        if proc.name() in ["jupyter-lab.exe", "jupyter.exe", "jupyter-notebook.exe", "chromedriver.exe"]:
-            print(proc)
-            os.kill(proc.pid, signal.SIGTERM)
-else:
-    os.killpg(os.getpgid(beakerx.pid), signal.SIGKILL)
-    kill_processes('jupyter')
-    kill_processes('webdriver')
-    kill_processes('java')
     
 if result:
     sys.exit(20)
